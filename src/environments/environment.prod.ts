4d3027f25e0c3585cdbe91654073ce986200ddaa
Paola Buggy <al226962@edu.uaa.mx>
2022-06-16 13:34:34 -0500

    credenciales firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDou8fFXO4ewYZNnlltcZ18PhomHD852xA",
+    authDomain: "pokepedia-1509f.firebaseapp.com",
+    projectId: "pokepedia-1509f",
+    storageBucket: "pokepedia-1509f.appspot.com",
+    messagingSenderId: "780770093383",
+    appId: "1:780770093383:web:350b0579b1566262bb0462"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDou8fFXO4ewYZNnlltcZ18PhomHD852xA",
+    authDomain: "pokepedia-1509f.firebaseapp.com",
+    projectId: "pokepedia-1509f",
+    storageBucket: "pokepedia-1509f.appspot.com",
+    messagingSenderId: "780770093383",
+    appId: "1:780770093383:web:350b0579b1566262bb0462"
+  }
 };
 
 /*
