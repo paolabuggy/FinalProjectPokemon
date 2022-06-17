ed3b5966e2bf3fc3ec78ccfcbc184240fdae2c33
Paola Buggy <al226962@edu.uaa.mx>
2022-06-17 18:31:23 -0500

    datos de cuenta

--- src/app/cuenta/cuenta.component.ts
+++ src/app/cuenta/cuenta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../servicios/auth.service';
 
 @Component({
   selector: 'app-cuenta',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CuentaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
   }
