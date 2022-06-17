03dd0ed2f08d51337eaee5732cd2cf3f0317b468
Paola Buggy <al226962@edu.uaa.mx>
2022-06-16 22:54:41 -0500

    nuevas funciones bd

--- src/app/servicios/firestore.service.ts
+++ src/app/servicios/firestore.service.ts
@@ -12,4 +12,26 @@ export class FirestoreService {
   public getPokemones() {
     return this.firestore.collection('pokemones').snapshotChanges();
   }
+
+  //Obtiene 1 pokemon por su id
+  public getPokemon(documentId: string) {
+    return this.firestore.collection('pokemones').doc(documentId).snapshotChanges();
+  }
+
+  //Crea un nuevo pokemon
+  public createPokemon(data: {nombre: string, tipo: string, foto: string}) {
+    return this.firestore.collection('pokemones').add(data);
+  }
+
+  //Actualiza un pokemon
+  public updatePokemon(documentId: string, data: any) {
+    return this.firestore.collection('pokemones').doc(documentId).set(data);
+  }
+
+  // Borra un pokemon
+  public deletePokemon(documentId: string) {
+    return this.firestore.collection('pokemones').doc(documentId).delete();
+  }
+
+  
 }
