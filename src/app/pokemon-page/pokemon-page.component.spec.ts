1ae56d6845d3936ca9c2d70fc30c4449383b07f4
TheVillalobos <cvm110801@gmail.com>
2022-06-18 18:23:03 -0500

    pagina individual de pokemones

---
+++ src/app/pokemon-page/pokemon-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PokemonPageComponent } from './pokemon-page.component';
+
+describe('PokemonPageComponent', () => {
+  let component: PokemonPageComponent;
+  let fixture: ComponentFixture<PokemonPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PokemonPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PokemonPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
