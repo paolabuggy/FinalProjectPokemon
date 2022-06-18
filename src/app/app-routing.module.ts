a0d9168c3973ad6f8ce262c064b11b7d651cf524
ChiefBoshy <smileykiller11@gmail.com>
2022-06-17 19:12:25 -0500

    commit suicide

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { FaqComponent } from './faq/faq.component';
 import { HomeComponent } from './home/home.component';
 import { PokemonesComponent } from './pokemones/pokemones.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-
+import { PokeGraphsComponent } from './Graficas/poke-graphs/poke-graphs.component';
 //Route Guard para poner en dashboard
 import { AuthGuard } from './servicios/guard/auth.guard';
 
@@ -16,7 +16,8 @@ const routes: Routes = [
   {path: 'faq', component: FaqComponent},
   {path: 'cuenta', component: CuentaComponent, canActivate: [AuthGuard] },
   {path: 'pokemon', component: PokemonesComponent},
-  {path: 'login' , component: LoginPageComponent}
+  {path: 'login' , component: LoginPageComponent},
+  {path: 'graphs',component:PokeGraphsComponent}
 ];
 
 @NgModule({
