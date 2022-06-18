af34847fd1c25bae20a77cbbd0c9c360f5113981
TheVillalobos <cvm110801@gmail.com>
2022-06-18 17:13:07 -0500

    Carrusel chido

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../servicios/firestore.service';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public pokemones: any = [];
+  
+  constructor(private firestoreService: FirestoreService) {
+    
+  }
 
   ngOnInit(): void {
+    this.firestoreService.getPokemones().subscribe((pokemonesSnapshot) => {
+      this.pokemones = [];
+      pokemonesSnapshot.forEach((pokemonData: any) => {
+        this.pokemones.push({
+          id: pokemonData.payload.doc.id,
+          data: pokemonData.payload.doc.data()
+        });
+      });
+    });
   }
 
 }
