8f6fd8a2dd1b04a4e275be3415afa1336a1f66cf
kvxxp3 <quezadakarla2406@gmail.com>
2022-06-19 14:54:40 -0500

    Mandar datos al codigo QR

--- src/app/cuenta/cuenta.component.ts
+++ src/app/cuenta/cuenta.component.ts
@@ -7,9 +7,9 @@ import { AuthService } from '../servicios/auth.service';
   styleUrls: ['./cuenta.component.css']
 })
 export class CuentaComponent implements OnInit {
-
   public EncontradoPoke:boolean=true;
   public EncontradoUser:boolean=true;
+
   constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
