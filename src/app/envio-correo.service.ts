1a3ec59a4cd30c57f58882a77d96829acc88801c
kvxxp3 <quezadakarla2406@gmail.com>
2022-06-19 18:25:19 -0500

    Borrado código comentado

--- src/app/envio-correo.service.ts
+++ src/app/envio-correo.service.ts
@@ -7,18 +7,9 @@ import { usermodel } from './objuser';
 })
 export class EnvioCorreoService {
   url='http://localhost:3000/send'
-  // postobj={
-  //   name:"karla",
-  //   lastname:"que",
-  //   email:"no",
-  //   message:"sixa"
-  // }
-  // json: string ="";
+  
   constructor(public httpClient: HttpClient) { }
 
-  // getJSON(url: string){
-  //   return this.httpClient.get(url);
-  // }
   send(obj: usermodel){
     return this.httpClient.post<any>(this.url, {'name':obj.name,'lastname':obj.lastname,'address':obj.address,'email':obj.email,'phone':obj.phone,'message':obj.message})
   }
