e3e1a5911689377f92ddf6cfd2147f0325c48bf2
TheVillalobos <cvm110801@gmail.com>
2022-06-19 00:24:31 -0500

    agregado busqueda

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/servicios/auth.service';
+import { Router } from '@angular/router';
+import { FirestoreService } from '../servicios/firestore.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,10 +9,49 @@ import { AuthService } from 'src/app/servicios/auth.service';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  
+  nombre?:string;
+public pokemones: any = [];
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, private router: Router, private firestoreService: FirestoreService) { 
+    this.firestoreService.getPokemones().subscribe((pokemonesSnapshot) => {
+      this.pokemones = [];
+      pokemonesSnapshot.forEach((pokemonData: any) => {
+        this.pokemones.push({
+          id: pokemonData.payload.doc.id,
+          data: pokemonData.payload.doc.data()
+        });
+      });
+    });
+  }
 
   ngOnInit(): void {
   }
+
+  buscarUnPoke(nombre:string){
+    this.router.navigate(['/home']);
+    
+    this.nombre=nombre;
+      this.searchUnPoke(this.nombre);
+    }
+        
+      
+    
+      searchUnPoke(nompoke:any):void{
+        this.pokemones.forEach((element:any)=>{
+          if (element.data.nombre==nompoke){
+            
+            
+            this.router.navigate(['/pokemonPage',element.data.nombre, element.data.foto]).then(() => {
+              window.location.reload();
+            });
+          
+            
+            return
+          }
+        });
+      }
+
+    
 
 }
